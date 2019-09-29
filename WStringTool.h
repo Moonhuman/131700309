89b526cee82af178207336b35d546a41618f61a5
Moonhuman <55131813+Moonhuman@users.noreply.github.com>
2019-09-29 23:04:57 +0800

    Delete WStringTool.h

--- WStringTool.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#pragma comment(lib, "comsuppw.lib")
-#include "stdafx.h"
-using namespace std;
-string wstring2string(const wstring& ws);
-wstring string2wstring(const string& s);
-void Initial();
