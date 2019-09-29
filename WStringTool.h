d31284aadab48bacf0682244684a6c03834fa1d2
Moonhuman <55131813+Moonhuman@users.noreply.github.com>
2019-09-29 23:27:21 +0800

    Delete WStringTool.h

--- WStringTool.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#pragma comment(lib, "comsuppw.lib")
-#include "stdafx.h"
-using namespace std;
-string UnicodeToUTF8(const wstring & wstr);
-wstring UTF8ToUnicode(const string & str);
-void Initial();
